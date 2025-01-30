cf43e8e4f5c58ea6e1963c60de024c42a730a5a8
Sakshi Patil <patilsakshi2002@gmail.com>
2025-01-29 21:55:11 -0700

    HW2

--- hw2/hw2_pi.py
+++ hw2/hw2_pi.py
@@ -1,11 +1,10 @@
 import numpy as np
 from scipy.integrate import quad
 
-def integrand(x):
-    return 1 / ((1 + x) * np.sqrt(x))
+def func(z):
+    return 1 / (np.sqrt((1 - z) * z))
 
-result, _ = quad(integrand, 0, np.inf)
+integral, error = quad(func, 0, 1)
 
-print(f"Pi is {result:.8f}")
-difference = np.pi - result
-print(f"Difference from numpy.pi is: {difference:.15f}")
+print("Pi is {:,.8f}".format(integral))
+print("Difference from numpy.pi is: {:,.15f}".format(np.pi - integral))
--- hw2/hw2_poly.py
+++ hw2/hw2_poly.py
@@ -1,38 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.integrate import quad  # Import the quad function
+from scipy.integrate import quad 
 
-# Define the polynomial function
 def polynomial(x, a, b, c):
     return a * x**2 + b * x + c
 
-# Define the integral function
 def integral_polynomial(r, a, b, c):
     result = np.zeros_like(r)
     for i, ri in enumerate(r):
         result[i], _ = quad(polynomial, 0, ri, args=(a, b, c))  # Use quad here
     return result
 
-# Define the range of r values
 r_values = np.arange(0, 5.01, 0.01)
 
-# First set of constants
 a1, b1, c1 = 2, 3, 4
 integral_values1 = integral_polynomial(r_values, a1, b1, c1)
 
-# Second set of constants
 a2, b2, c2 = 2, 1, 1
 integral_values2 = integral_polynomial(r_values, a2, b2, c2)
 
-# Plot the results
 plt.plot(r_values, integral_values1, label=f'a={a1}, b={b1}, c={c1}')
 plt.plot(r_values, integral_values2, label=f'a={a2}, b={b2}, c={c2}')
 
-# Add labels, title, and legend
 plt.xlabel('r')
-plt.ylabel('Integral Value')
+plt.ylabel('ax^2 + bx + c')
 plt.title('hw2 poly')
 plt.legend()
 
-# Show the plot
 plt.show()
